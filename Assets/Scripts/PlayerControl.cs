ebb27c209cb0bf143601fa25ec35a9d9238ddc2e
Josiah Gray <josiah.gray.14@cnu.edu>
2018-02-17 18:07:42 -0500

    isGrounded finally done, some animations are still janky
    Need to work on transitioning between animations. The dash animation has been completely broken somehow, too.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,70 +4,70 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
+    private Rigidbody2D playerBody;
+    private LayerMask groundLayer;
     public int playerSpeed = 10;
     private bool facingRight = true;
     private int playerJumpNum = 0;
-    public int playerJumpPower = 1250;
+    private int playerJumpPower = 1250;
     public int DashCooldown = 0;
-    public int DashImpulse = 0;
+    private int DashImpulse = 0;
     public int playerDashPower = 1000;
     private float moveX;
+    public int attackLength;
 
     private Animator animator;
     private Animation playerAnimation;
     public float speed = 1f;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
+        playerBody = gameObject.GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         playerAnimation = GetComponent<Animation>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        groundLayer = LayerMask.GetMask( "Ground" );
+    }
+
+    // Update is called once per frame
+    void Update() {
         PlayerMove();
     }
-    //comment
 
+    //we can change priority of animations in PlayerWarrior animator
+    //as well as interupts
+    //currently this complicates things as the run animation will always take over
+    //even if another animation is trying to play
     void PlayerMove() {
         moveX = Input.GetAxis( "Horizontal" );
-        
-        //we can change priority of animations in PlayerWarrior animator
-        //as well as interupts
-        //currently this complicates things as the run animation will always take over
-        //even if another animation is trying to play
 
-        //if (playerIsMoving())
-        //{
-        //    animator.Play("PlayerWarrior_Move");
-        //}
-        
+        if ( isGrounded() == true ) {
+            playerJumpNum = 0;
+
+            if ( playerIsMoving() && DashImpulse == 0 ) {
+                animator.Play( "PlayerWarrior_Move" );
+            } else {
+                animator.Play( "PlayerWarrior_Idle" );
+            }
+        }
 
         if ( DashCooldown > 0 ) {
             DashCooldown--;
         }
+
         if ( DashImpulse > 0 ) {
-            GetComponent<Rigidbody2D>().AddForce( new Vector2( playerDashPower, 0 ), ForceMode2D.Force );
+            playerBody.AddForce( new Vector2( playerDashPower, 0 ), ForceMode2D.Force );
             DashImpulse--;
         } else if ( DashImpulse < 0 ) {
-            GetComponent<Rigidbody2D>().AddForce( new Vector2( -playerDashPower, 0 ), ForceMode2D.Force );
+            playerBody.AddForce( new Vector2( -playerDashPower, 0 ), ForceMode2D.Force );
             DashImpulse++;
         }
 
-        if ( (Input.GetButtonDown( "Jump" ) || Input.GetButtonDown( "Jump (Controller)" )) && playerJumpNum != 2) {
+        if ( ( Input.GetButtonDown( "Jump" ) || Input.GetButtonDown( "Jump (Controller)" ) ) && playerJumpNum < 1 ) {
             Jump();
-            playerJumpNum++;
         }
-
-        if (isGrounded() == true)
-        {
-            playerJumpNum = 0;
-        }
-
 
         if ( ( Input.GetButtonDown( "Dash" ) || Input.GetAxis( "Dash (Controller)" ) == 1 ) && DashCooldown == 0 ) {
             Dash();
-            
         }
 
         if ( moveX > 0.0f && facingRight == false ) {
@@ -75,48 +75,47 @@ public class PlayerControl : MonoBehaviour {
         } else if ( moveX < 0.0f && facingRight == true ) {
             FlipPlayer();
         }
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2( moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y );
+        playerBody.velocity = new Vector2( moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y );
 
-        if (Input.GetMouseButtonDown(0))
-        {
+        if ( Input.GetMouseButtonDown( 0 ) ) {
             Attack1();
         }
 
     }
-
+    
     //funct to check if char is grounded. not done/working
-    bool isGrounded()
-    {
-        if (Physics2D.Raycast(transform.position, Vector2.down, 1.0f).collider != null)
-        {
-            return true;
-        }
+    bool isGrounded() {
+        Vector2 pos = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 1.0f;
+        RaycastHit2D hit = Physics2D.Raycast( pos, direction, distance, groundLayer );
+        //Debug.DrawRay( pos, direction, Color.green );
+        return ( hit.collider != null );
 
-        return false;
     }
 
     void Jump() {
-        GetComponent<Rigidbody2D>().AddForce( Vector2.up * playerJumpPower );
-        animator.Play("PlayerWarrior_Jump");
+        playerBody.AddForce( Vector2.up * playerJumpPower );
+        animator.Play( "PlayerWarrior_Jump" );
+        playerJumpNum++;
     }
 
     void Dash() {
         DashCooldown = 50;
         if ( facingRight ) {
-            GetComponent<Rigidbody2D>().AddForce( new Vector2 ( playerDashPower, 0 ), ForceMode2D.Force );
+            playerBody.AddForce( new Vector2( playerDashPower, 0 ), ForceMode2D.Force );
             DashImpulse = 10;
 
         } else {
-            GetComponent<Rigidbody2D>().AddForce( new Vector2( -playerDashPower, 0 ), ForceMode2D.Force );
+            playerBody.AddForce( new Vector2( -playerDashPower, 0 ), ForceMode2D.Force );
             DashImpulse = -10;
-            
-            
         }
+        animator.Play( "HeroWarrior_Skill2" );
     }
 
-    void Attack1()
-    {
-        animator.Play("HeroWarrior_Attack_part1");
+    void Attack1() {
+        animator.Play( "HeroWarrior_Attack_part3" );
+        attackLength = 20;
     }
 
     void FlipPlayer() {
@@ -126,12 +125,7 @@ public class PlayerControl : MonoBehaviour {
         transform.localScale = localScale;
     }
 
-    bool playerIsMoving()
-    {
-        if (moveX != 0)
-        {
-            return true;
-        }
-        return false;
+    bool playerIsMoving() {
+        return ( moveX != 0 );
     }
 }
