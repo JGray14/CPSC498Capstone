ce9e9fc80e6066e69400afd75c9f43062987e4b1
Josiah Gray <josiah.gray.14@cnu.edu>
2018-02-28 18:30:35 -0500

    Spikes reset level

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spikes : MonoBehaviour {
 
+    private GameObject obj;
+
     void OnTriggerEnter2D( Collider2D other ) {
-        Destroy( other.gameObject );
+        obj = other.gameObject;
+        if ( obj.tag == "Player" ) {
+            //PlayerIsKill( other );
+            SceneManager.LoadScene( "Testing" );
+        } else {
+            Destroy( other.gameObject );
+        }
+        
     }
-	// Use this for initialization
-	void Start () {
+
+    IEnumerator PlayerIsKill( Collider2D other ) {
+        Destroy( other.gameObject );
+        yield return new WaitForSeconds( 3 );
+        SceneManager.LoadScene( "Testing" );
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
