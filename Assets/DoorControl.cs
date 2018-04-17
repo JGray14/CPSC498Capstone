b43787333277261119cb2f8e88807489857572cf
Josiah Gray <josiah.gray.14@cnu.edu>
2018-04-17 02:15:02 -0400

    Think everything's done

--- Assets/DoorControl.cs
+++ Assets/DoorControl.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class DoorControl : MonoBehaviour {
 
+    GameObject door;
+    bool closed;
+
 	// Use this for initialization
 	void Start () {
-		
+        door = transform.parent.gameObject;
+        closed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public IEnumerator CloseDoor() {
+        for ( float i = 0; i < 2.9; i += 0.1f ) {
+            yield return new WaitForSeconds( 0.025f );
+            door.transform.position = new Vector2(door.transform.position.x, door.transform.position.y - 0.1f );
+        }
+    }
+    private void OnTriggerEnter2D( Collider2D other ) {
+        if ( other.tag == "Player" && !closed ) {
+            closed = true;
+            StartCoroutine( CloseDoor() );
+        }
+    }
 }
